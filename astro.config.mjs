1f085f8d60367fe756fcac9965f98c57f83d80e5
FotisP <fotispastrakis@gmail.com>
2026-08-08 20:35:51 +0300

    fix(config): explicitly set static output mode in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  output: 'static',
   integrations: [mdx()],
   adapter: cloudflare({
     imageService: 'cloudflare',
